b42d95b3e930a20e8b1182cab8fe1cc2d5a40410
garciacarlosrodrigo <garciacarlosrodrigo@gmail.com>
2022-11-14 20:30:14 -0300

    Teste primeiro comit

---
+++ primeiro.js
@@ -0,0 +1,15 @@
+
+let a = 7
+let b = 94
+let x = 0
+
+console.log(" O valor da variavel a é " + a)
+console.log(" O valor da variavel b é " + b)
+
+x = a
+a = b
+b = x
+
+console.log(" Agora o valor da variavel a é " + a)
+console.log(" Agora o valor da variavel b é " + b)
+
