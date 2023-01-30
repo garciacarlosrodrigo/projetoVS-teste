7be1fa8495a3346b7710205a3e2f4ec8d67ae45b
garciacarlosrodrigo <garciacarlosrodrigo@gmail.com>
2023-01-30 19:04:45 -0300

    versao 03

--- primeiro.js
+++ primeiro.js
@@ -9,7 +9,7 @@ console.log(" O valor da variavel b é " + b)
 x = a
 a = b
 b = x
-c = 1 
+c = 2
 
 console.log(" Agora o valor da variavel a é " + a)
 console.log(" Agora o valor da variavel b é " + b)
